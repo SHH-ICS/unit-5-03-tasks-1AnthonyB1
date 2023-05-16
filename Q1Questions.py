37359a7f599fed5ecee4141ef3b32debf70c57c2
Anthony Bengtsson <124379293+1AnthonyB1@users.noreply.github.com>
2023-05-16 12:12:00 +0000

    Q1 and Q2 almost done

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,17 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+import json
+
+Question = input("\nWhat is your multiple choice question?\n(Example Question: What is 1 + 1)\nType your own question here: ")
+
+print('')
+A = []
+for i in range(4):
+    A.append(input(f"Answer {i + 1}: "))
+
+Correct = int(input("\nCorrect Index: "))
+
+myJson = {"Question":Question, "Answers":A, "Correct":Correct}
+
+with open("Question.json", "w") as f:
+    f.write(json.dumps(myJson))
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,20 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+import json
+
+with open("Question.json", "r") as f:
+    Data = json.loads(f.read())
+
+print("")
+print(Data["Question"])
+print("\nAnswer 1:",(Data["Answers"][0]))
+print("Answer 2:",(Data["Answers"][1]))
+print("Answer 3:",(Data["Answers"][2]))
+print("Answer 4:",(Data["Answers"][3]))
+
+b = int(input("\nWhat is the correct answer: "))
+
+if b == (Data["Correct"]):
+    print("Correct!")
+elif b != (Data["Correct"]):
+    print("Incorrect!")
